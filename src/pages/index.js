1d12ea8400316e29662c80b93b95499c3fa4e5e0
yanbinkwan <ybguan@ronglian.com>
2021-09-28 18:06:17 +0800

    init project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
+import Layout from '../components/Layout'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <Layout>
+    THIS IS HOME
+  </Layout>
 }
